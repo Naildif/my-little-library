a3bc9facdcf0f25433df40be104482ec09270894
Nailyn DFl <nailyn.diazf@gmail.com>
2024-09-09 19:33:00 -0500

    Create entity and repository layer

---
+++ src/main/java/com/library/entity/Book.java
@@ -0,0 +1,77 @@
+package com.library.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false)
+    private String title;
+    private String genre;
+    private double price;
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    public Book(){}
+    public Book(String author, String title, String genre, double price, LocalDate releaseDate){
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.releaseDate = releaseDate;
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
